4e4a390dd9258a6a65fafe103ed1f27061b91adc
SoftwareEngineer01 <sistemsistemaweb@gmail.com>
2022-01-17 11:15:13 -0500

    Se crea ComentarioResource

---
+++ app/Http/Resources/ComentarioResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ComentarioResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'contenido' => $this->contenido,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
